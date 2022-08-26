7579411f9e6747ffc8f6bc9ce957a9153dbb7de2
Henrique Naimaier <coubron@yahoo.com.br>
2022-08-26 14:44:51 -0300

    Added db for production

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO users(username, password) VALUES ('user', '$2a$12$/d/JGvMiPcLZgHL9/la70.B/Vkv.TYbImJlmlLmooB5OH0XnEaN72');
+
+INSERT INTO receitas(data, descricao, valor) VALUES ('2022-08-01', 'salario', 10000);
+INSERT INTO receitas(data, descricao, valor) VALUES ('2022-08-01', 'freelance', 1200);
+
+INSERT INTO despesas(data, descricao, valor, categoria) VALUES ('2022-08-05', 'mercado', 700, 'ALIMENTAÇÃO');
+INSERT INTO despesas(data, descricao, valor, categoria) VALUES ('2022-08-06', 'acougue', 200, 'ALIMENTAÇÃO');
+INSERT INTO despesas(data, descricao, valor, categoria) VALUES ('2022-08-31', 'restaurante', 300, 'ALIMENTAÇÃO');
+INSERT INTO despesas(data, descricao, valor, categoria) VALUES ('2022-08-10', 'combustivel', 1000, 'TRANSPORTE');
+INSERT INTO despesas(data, descricao, valor, categoria) VALUES ('2022-08-05', 'luz', 300, 'MORADIA');
+INSERT INTO despesas(data, descricao, valor, categoria) VALUES ('2022-08-05', 'aluguel', 1500, 'MORADIA');
+INSERT INTO despesas(data, descricao, valor, categoria) VALUES ('2022-08-05', 'internet', 100, 'MORADIA');
